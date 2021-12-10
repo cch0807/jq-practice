add6bb2cb9d8d75377204f5c01a399999ee06c4b
[cch] <[ckdgus11021@mail.com]>
2021-12-10 14:56:39 +0900

    new

---
+++ tab.js
@@ -0,0 +1,20 @@
+$('.tab-button').eq(0).click(function(){
+  $('.tab-button').removeClass('active');
+  $('.tab-content').removeClass('show');
+  $('.tab-button').eq(0).addClass('active');
+  $('.tab-content').eq(0).addClass('show');
+})
+
+$('.tab-button').eq(1).click(function(){
+  $('.tab-button').removeClass('active');
+  $('.tab-content').removeClass('show');
+  $('.tab-button').eq(1).addClass('active');
+  $('.tab-content').eq(1).addClass('show');
+})
+
+$('.tab-button').eq(2).click(function(){
+  $('.tab-button').removeClass('active');
+  $('.tab-content').removeClass('show');
+  $('.tab-button').eq(2).addClass('active');
+  $('.tab-content').eq(2).addClass('show');
+})
